a0ea0f0dc17fb5706838123b0c3d2dc1f6d4e8b2
Paul Chote <paul@chote.net>
2011-12-28 01:38:01 -0500

    Add GPL3 license

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// This file is part of webglrpg-client, Copyright (C) 2011 Paul Chote
+// You can redistribute and/or modify it under the terms of version 3 of the
+// GNU General Public License, as published by the Free Software Foundation.
+// See LICENSE.html for the license terms.
+
 var gl;
 var shaderProgram;
 var mvMatrix = mat4.create();
--- map.js
+++ map.js
@@ -1,3 +1,8 @@
+// This file is part of webglrpg-client, Copyright (C) 2011 Paul Chote
+// You can redistribute and/or modify it under the terms of version 3 of the
+// GNU General Public License, as published by the Free Software Foundation.
+// See LICENSE.html for the license terms.
+
 Map = function() {
     this.mapSize = [5, 5];
     this.tileset = new Tileset();
